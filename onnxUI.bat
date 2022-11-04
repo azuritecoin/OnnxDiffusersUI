2f41c141493fd30da07056233b246af855273848
Sam To <dummi_boi@yahoo.ca>
2022-11-04 01:32:35 -0700

    Updated onnxUI.bat
    this batch will only setup the envrionment and handle updates

--- onnxUI.bat
+++ onnxUI.bat
@@ -2,8 +2,6 @@
 
 set first_run=0
 set venv_path="virtualenv"
-set main_script="onnxUI.py"
-set default_model="runwayml/stable-diffusion-v1-5"
 
 :: check if programs are installed
 python --version 1> NUL 2> NUL
@@ -14,59 +12,53 @@ pip --version 1> NUL 2> NUL
 if %errorlevel% NEQ 0 goto NoPip
 
 :: parse the parameters
-set model=
-set cpu_only=0
+set update=0
 
 :ArgParseLoop
 if not "%1"=="" (
-    if "%1"=="-model" (
-        set model=%2
-        shift
-    )
-    if "%1"=="-cpu-only" (
-        set cpu_only=1
-        shift
+    if "%1"=="-update" (
+        set update=1
     )
     shift
     goto ArgParseLoop
 )
-if "%model%"=="" set model=%default_model%
-
-echo %model%
-echo %cpu_only%
 
 :: check if virtual environment is present, else create it and install requirements
-if exist %venv_path%\Scripts\activate.bat goto SkipCreateVEnv
+if exist %venv_path%\Scripts\activate.bat goto ActivateVirtEnv
 
 echo no %venv_path% folder detected, creating virtual environment
 set first_run=1
 python -m venv %venv_path%
 
-:SkipCreateVEnv
+:ActivateVirtEnv
 call %venv_path%\Scripts\activate.bat
 :: using wget from python library instead of standalone wget for Windows
 if not exist %venv_path%\Lib\site-packages\wget.py pip install wget
 
-if %first_run% NEQ 0 goto FirstRunInstall
+:: for the first run, get requirements and install packages
+if %first_run%==0 goto ScriptDownload
 
-:AfterFirstRun
-:: check for token
-rem onnxUI.py --model="stable_diffusion_onnx-v1.5"
-
-pause
-goto:eof
-
-
-:FirstRunInstall
-:: download files from OnnxDiffusersUI GitHub
 echo installing Python packages
-
-if not exist requirements.txt %venv_path%\Lib\site-packages\wget.py https://raw.githubusercontent.com/azuritecoin/OnnxDiffusersUI/main/requirements.txt
+if not exist requirements.txt python -m wget https://raw.githubusercontent.com/azuritecoin/OnnxDiffusersUI/main/requirements.txt
 pip install -r requirements.txt
 
-rem if not exist onnxUI.py %venv_path%\Lib\site-packages\wget.py https://raw.githubusercontent.com/azuritecoin/OnnxDiffusersUI/main/onnxUI.py
+:ScriptDownload
+if not exist onnxUI.py python -m wget https://raw.githubusercontent.com/azuritecoin/OnnxDiffusersUI/main/onnxUI.py
 
-goto AfterFirstRun
+:: update the python packages and redownload onnxUI.py
+if %update%==0 goto FinishSetup
+if exist requirements.txt del requirements.txt
+python -m wget https://raw.githubusercontent.com/azuritecoin/OnnxDiffusersUI/main/requirements.txt
+pip install -U -r requirements.txt
+
+if exist onnxUI.py del onnxUI.py
+python -m wget https://raw.githubusercontent.com/azuritecoin/OnnxDiffusersUI/main/onnxUI.py
+
+:FinishSetup
+deactivate
+echo setup complete
+pause
+goto:eof
 
 
 :NoPython
