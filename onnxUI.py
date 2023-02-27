95fb45e7fd73fa95fda9d7723b34a7b8a8727565
xzuyn <16216325+xzuyn@users.noreply.github.com>
2023-02-27 18:37:23 -0500

    set original steps for txt2img as well

--- onnxUI.py
+++ onnxUI.py
@@ -711,6 +711,8 @@ def generate_click(
 
     # run the pipeline with the correct parameters
     if current_tab == 0:
+        original_steps = steps_t0
+        
         images, status = run_diffusers(
             prompt_t0,
             neg_prompt_t0,
